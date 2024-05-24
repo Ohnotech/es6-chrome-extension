179a226c6c065344e85df6faaae5252ca5af95a0
Ohnotech <hexabee76@gmail.com>
2024-05-24 04:58:38 -0700

    feat: import modules asynchronously

--- content/content.js
+++ content/content.js
@@ -1,35 +1,37 @@
 'use strict';
 
-// Content script is injecting a script with ES6 module support into the current page
-// const script = document.createElement('script')
-// script.setAttribute('type', 'module')
-// script.setAttribute('src', chrome.runtime.getURL('/content/content-main.js'))
-// const head = document.head || document.getElementsByTagName('head')[0] || document.documentElement
-// head.insertBefore(script, head.lastChild)
-
 try {
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (request.message) {
+    (async () => {
+        const src = chrome.runtime.getURL("content/modules.js");
+        import(src).then((jsModule => {
+            console.log("jsModules loaded", jsModule);
+            Promise.all([
+                import('./alert-module.js'),
+                import('./log-module.js')
+            ]).then(([
+                {alertMessage}, 
+                {logMessage}
+            ]) => {
+                console.log("alertMessage, logMessage module loaded");
+
+                chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+                    if (request.message) {
+                        
+                        console.log(` -----Message received from popup.js:, ${request.message}`);
+                        if(request.message === "log-message")
+                            logMessage("Log message in content.js");
+                        else if(request.message === "alert-message")
+                            alertMessage("Alert message in content.js");
             
-            console.log('Message received from popup.js:', request.message);
+                        sendResponse({
+                            received: true
+                        });
+                    }
+                });
+            })
+        }))
 
-            // (async () => {
-                const src = chrome.runtime.getURL("content/content-module.js");
-                import(src).then((jsModule => {
-                    if(request.message === "log-message")
-                        jsModule.logMessage("Log message in content.js");
-                    else if(request.message === "alert-message")
-                        jsModule.alertMessage("Alert message in content.js");
-                }))
-
-            // })();
-
-            sendResponse({
-                received: true
-            });
-        }
-    });
-
+    })();
     console.log("content loaded");
 } catch (error) {
     console.error(error);
